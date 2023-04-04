45076b98a3e199e56875595b3dd203b0470b800d
Billy024 <paavanan12@gmail.com>
2023-04-05 06:45:40 +0800

    added test for canUseOfferCode function

--- services/discount/canUseOfferCode.js
+++ services/discount/canUseOfferCode.js
@@ -5,18 +5,29 @@ export function canUseOfferCode(
   available_offer_codes
 ) {
   let validity = available_offer_codes
-    .filter((offerCode) => offerCode.name == offer_code)
+    .filter((offerCode) => {
+      return offerCode.name == offer_code;
+    })
     .map((filteredOfferCode) => {
+      console.log(
+        filteredOfferCode.minDistance,
+        "filteredOfferCode.minDistance"
+      );
+
       if (weight < filteredOfferCode.minWeight) {
+        console.log("1");
         return false;
       }
       if (weight > filteredOfferCode.maxWeight) {
+        console.log("2");
         return false;
       }
       if (distance < filteredOfferCode.minDistance) {
+        console.log("3");
         return false;
       }
       if (distance > filteredOfferCode.maxDistance) {
+        console.log("4");
         return false;
       }
       return true;
--- test/utilDiscount.test.js
+++ test/utilDiscount.test.js
@@ -84,48 +84,4 @@ describe("discountUtils", function () {
       done();
     });
   });
-  describe("check if can use Offer Code", function () {
-    it("should get false based on offer Code OFR001 parameters", function (done) {
-      const weight1 = 10;
-      const distance1 = 10;
-      const offerCode1 = "OFR001";
-      const validity1 = canUseOfferCode(
-        weight1,
-        distance1,
-        offerCode1,
-        offerCodes
-      );
-      const expectedValidity1 = false;
-      expect(validity1).to.equal(expectedValidity1);
-      done();
-    });
-    it("should get true based on offer Code OFR002 parameters", function (done) {
-      const weight2 = 150;
-      const distance2 = 100;
-      const offerCode2 = "OFR002";
-      const validity2 = canUseOfferCode(
-        weight2,
-        distance2,
-        offerCode2,
-        offerCodes
-      );
-      const expectedValidity2 = true;
-      expect(validity2).to.equal(expectedValidity2);
-      done();
-    });
-    it("should get true based on offer Code OFR003 parameters", function (done) {
-      const weight3 = 20;
-      const distance3 = 100;
-      const offerCode3 = "OFR003";
-      const validity3 = canUseOfferCode(
-        weight3,
-        distance3,
-        offerCode3,
-        offerCodes
-      );
-      const expectedValidity3 = true;
-      expect(validity3).to.equal(expectedValidity3);
-      done();
-    });
-  });
 });
