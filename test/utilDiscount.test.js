1cc13512b26b5dbb89db80cde140160404c3557c
Billy024 <paavanan12@gmail.com>
2023-04-05 06:54:58 +0800

    refactored test for utilDiscount

--- test/utilDiscount.test.js
+++ test/utilDiscount.test.js
@@ -11,53 +11,6 @@ import { offerCodes } from "../offerCodes.js";
 const expect = _expect;
 
 describe("discountUtils", function () {
-  // describe("get Discount and Total Cost", function () {
-  //   it("should get discountedCost object from passing in array of packages, base cost and available offer codes", function (done) {
-  //     const packagesArray = [
-  //       new _package("PKG1", 100, 100, "OFR001"), //should get discount OFR001
-  //       new _package("PKG2", 100, 201, "OFR001"), //exceeding max distance - should not get discount
-  //       new _package("PKG3", 50, 251, "ABCDEF"), //incorrect offercode - should not get discount
-  //     ];
-  //     const base_cost = 90;
-  //     const discountedCostObject = getDiscountAndTotalCost(
-  //       packagesArray,
-  //       base_cost,
-  //       offerCodes
-  //     );
-  //     const deliveryCost1 = getDeliveryCost(
-  //       base_cost,
-  //       packagesArray[0].weight,
-  //       packagesArray[0].distance
-  //     );
-  //     const deliveryCost2 = getDeliveryCost(
-  //       base_cost,
-  //       packagesArray[1].weight,
-  //       packagesArray[1].distance
-  //     );
-  //     const deliveryCost3 = getDeliveryCost(
-  //       base_cost,
-  //       packagesArray[2].weight,
-  //       packagesArray[2].distance
-  //     );
-  //     let expectedDiscountedCostObject = [];
-
-  //     expectedDiscountedCostObject.push(
-  //       new discountedCost(
-  //         packagesArray[0].id,
-  //         (10 / 100) * deliveryCost1,
-  //         deliveryCost1 - (10 / 100) * deliveryCost1
-  //       )
-  //     );
-  //     expectedDiscountedCostObject.push(
-  //       new discountedCost(packagesArray[1].id, 0, deliveryCost2)
-  //     );
-  //     expectedDiscountedCostObject.push(
-  //       new discountedCost(packagesArray[2].id, 0, deliveryCost3)
-  //     );
-  //     expect(discountedCostObject).to.deep.equal(expectedDiscountedCostObject);
-  //     done();
-  //   });
-  // });
   describe("get Delivery Cost", function () {
     it("should get delivery cost from passing in base delivery cost, weight of package and distance", function (done) {
       const base_delivery_cost = 90;
