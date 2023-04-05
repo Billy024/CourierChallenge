4658c6b54405ec1210cb175d5501c15ee17f0301
Billy024 <paavanan12@gmail.com>
2023-04-05 15:53:46 +0800

    added test for getPackagesData

--- services/packages/getPackagesData.js
+++ services/packages/getPackagesData.js
@@ -5,11 +5,12 @@ import { promptQuestion } from "../terminal/promptQuestion.js";
 export async function getPackagesData(
   no_of_packages,
   package_question,
-  input_quantity
+  input_quantity,
+  promptFn = promptQuestion()
 ) {
   let packagesData = [];
   for (let i = 0; i < no_of_packages; i++) {
-    const packageAnswer = await promptQuestion(package_question);
+    const packageAnswer = await promptFn(package_question);
     const packageArray = splitStringtoArray(packageAnswer, input_quantity);
     const packageData = getPackageDetail(packageArray);
     packagesData.push(packageData);
---
+++ test/getPackagesData.test.js
@@ -0,0 +1,39 @@
+import { expect as _expect } from "chai";
+import { _package } from "../models/package.js";
+import { getPackagesData } from "../services/packages/getPackagesData.js";
+import { PACKAGE_QUESTION } from "../constants.js";
+import { getPackageDetail } from "../services/packages/getPackageDetail.js";
+
+const expect = _expect;
+
+describe("getPackageData", function () {
+  it("should get get packages from user input", async function () {
+    const numberOfPackages = 1;
+    const packageQuestion = PACKAGE_QUESTION;
+    const inputQuantity = 4;
+    const promptAnswer1 = "PKG1";
+    const promptAnswer2 = "40";
+    const promptAnswer3 = "60";
+    const promptAnswer4 = "OFR001";
+
+    const promptAnswer = `${promptAnswer1} ${promptAnswer2} ${promptAnswer3} ${promptAnswer4}`;
+    async function promptFn() {
+      return [promptAnswer];
+    }
+
+    const result = await getPackagesData(
+      numberOfPackages,
+      packageQuestion,
+      inputQuantity,
+      promptFn
+    );
+    expect(result).to.deep.equal([
+      getPackageDetail([
+        promptAnswer1,
+        promptAnswer2,
+        promptAnswer3,
+        promptAnswer4,
+      ]),
+    ]);
+  });
+});
