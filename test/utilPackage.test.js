0ccab95b01008c81b3afa0afd27bc47aafd2b927
Billy024 <paavanan12@gmail.com>
2023-04-05 16:21:08 +0800

    added test for sortPackagesByWeightAndDistance

--- test/utilPackage.test.js
+++ test/utilPackage.test.js
@@ -1,8 +1,9 @@
 import { expect as _expect } from "chai";
-import { packagesWithDeliveryTime } from "../models/packagesWithDeliveryTime.js";
-import { findMaxDeliveryTime } from "../services/deliverySorting/findMaxDeliveryTime.js";
 import { _package } from "../models/package.js";
-import { filterOverweightPackages } from "../services/packages/utilPackage.js";
+import {
+  filterOverweightPackages,
+  sortPackagesByWeightAndDistance,
+} from "../services/packages/utilPackage.js";
 
 const expect = _expect;
 describe("utilsPackage", function () {
@@ -22,6 +23,22 @@ describe("utilsPackage", function () {
         packagesArray,
         maxCarriableWeight
       );
+      expect(result).to.deep.equal(expectedPackages);
+    });
+  });
+  describe("sortPackagesByWeightAndDistance", function () {
+    it("should sort packages by weight and distance", function () {
+      const packagesArray = [
+        new _package("PKG1", 50, 20, "OFR001"),
+        new _package("PKG2", 60, 20, "OFR001"),
+        new _package("PKG3", 60, 30, "OFR001"),
+      ];
+      const expectedPackages = [
+        new _package("PKG2", 60, 20, "OFR001"),
+        new _package("PKG3", 60, 30, "OFR001"),
+        new _package("PKG1", 50, 20, "OFR001"),
+      ];
+      const result = sortPackagesByWeightAndDistance(packagesArray);
       expect(result).to.deep.equal(expectedPackages);
     });
   });
