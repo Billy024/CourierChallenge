1ad8f0618a62d17e8896f7b2a2db9eca9c6b57d3
Billy024 <paavanan12@gmail.com>
2023-04-05 16:28:09 +0800

    added test for getVehicleData

--- services/vehicles/getVehicleData.js
+++ services/vehicles/getVehicleData.js
@@ -2,8 +2,12 @@ import { promptQuestion } from "../terminal/promptQuestion.js";
 import { splitStringtoArray } from "../terminal/utilTerminal.js";
 import { getVehicleDetail } from "./getVehicleDetail.js";
 
-export async function getVehicleData(vehicle_question, input_quantity) {
-  const vehicleAnswer = await promptQuestion(vehicle_question);
+export async function getVehicleData(
+  vehicle_question,
+  input_quantity,
+  promptFn = promptQuestion()
+) {
+  const vehicleAnswer = await promptFn(vehicle_question);
   const vehicleStringArray = splitStringtoArray(vehicleAnswer, input_quantity);
   return getVehicleDetail(vehicleStringArray);
 }
---
+++ test/getVehicleData.test.js
@@ -0,0 +1,31 @@
+import { expect as _expect } from "chai";
+import { DELIVERY_QUESTION, VEHICLE_QUESTION } from "../constants.js";
+import { getDeliveryDetail } from "../services/delivery/getDeliveryDetail.js";
+import { getDeliveryData } from "../services/delivery/getDeliveryData.js";
+import { getVehicleDetail } from "../services/vehicles/getVehicleDetail.js";
+import { getVehicleData } from "../services/vehicles/getVehicleData.js";
+
+const expect = _expect;
+
+describe("getVehicleData", function () {
+  it("should get vehicle array from mocked user input", async function () {
+    const vehicleQuestion = VEHICLE_QUESTION;
+    const inputQuantity = 3;
+    const promptAnswer1 = "2";
+    const promptAnswer2 = "100";
+    const promptAnswer3 = "200";
+    const promptAnswer = `${promptAnswer1} ${promptAnswer2} ${promptAnswer3}`;
+    async function promptFn() {
+      return [promptAnswer];
+    }
+
+    const result = await getVehicleData(
+      vehicleQuestion,
+      inputQuantity,
+      promptFn
+    );
+    expect(result).to.deep.equal(
+      getVehicleDetail([promptAnswer1, promptAnswer2, promptAnswer3])
+    );
+  });
+});
